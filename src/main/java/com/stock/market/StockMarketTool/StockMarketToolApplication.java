144edeed1c14cf1407ac0dc92f79790846e42e73
bamb <bamb.bakang@gmail.com>
2021-04-18 15:35:27 -0400

    Initializes the project with spring boot project generator.

---
+++ src/main/java/com/stock/market/StockMarketTool/StockMarketToolApplication.java
@@ -0,0 +1,13 @@
+package com.stock.market.StockMarketTool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockMarketToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockMarketToolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stock/market/StockMarketTool/StockMarketToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stock.market.StockMarketTool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockMarketToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
